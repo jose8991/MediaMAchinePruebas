7dc337d599932151da10d011c817df0a224826c0
jose <joseluisgm1998@gmail.com>
2022-04-05 07:45:12 -0500

    prueba ok

--- wpclonepython/actor_cloning.py
+++ wpclonepython/actor_cloning.py
@@ -20,18 +20,21 @@ def lista_dns(IDNS=os.getenv('ID_DNS'),EMAIL=os.getenv('EMAIL'),KEY=os.getenv('K
         list.append(a)
     return list
 ListaDns=lista_dns()
+print("guardo dns correctamente en la lista")
 #agregamos los elemetos del csv en la lista
 LocationFile=f"{os.getenv('MMHOME')}/settings/InstaPruebasActor.csv"
 df=pd.read_csv(LocationFile, header=None)
 ElementListCsv=df[1].values
+print("leyo correctamente el archivo csv")
 
 #iteramos todos los elementos de la lista csv y los que no existen llamamos la funcion new_wordpresss para crear el wordpress
 for i in ElementListCsv:
+    print("entro correctamente al ciclo for")
     iteracion=os.path.exists(f'/etc/nginx/sites-available/{i}')
     EstaEnDns=i in ListaDns
     if (iteracion or EstaEnDns):
         print(i) 
     else:
-        funcion_new_wordpress.new_wordpress(i,"Jose123-")  
+        funcion_new_wordpress.new_wordpress(i,os.getenv("WP_DB_PWD"))  
     
 
